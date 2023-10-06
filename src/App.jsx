65ccb2fb61437f27edb34b76846b90fab98eeb64
pelinergul <pelinxer@gmail.com>
2023-10-06 20:24:56 +0300

    components oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { TaskCreate } from './components/TaskCreate'
+import { TaskList } from './components/TaskList'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <TaskCreate /> 
+      <h1>Görevler</h1>
+      <TaskList />
     </>
   )
 }
---
+++ src/components/TaskCreate.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const TaskCreate = () => {
+  return (
+    <div>
+        <h3>Lütfen Task Ekleyiniz</h3>
+        <form>
+            <label>Başlık Giriniz</label>
+            <input />
+            <label>Task Giriniz</label>
+            <textarea />
+            <button>Oluştur</button>
+        </form>
+    </div>
+  )
+}
